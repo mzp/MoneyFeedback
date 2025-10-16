27eef4969085c664b731acdc0681342b1867ef88
Hiro Mizuno <me@mzp.jp>
2025-10-15 23:36:31 -0700

    Setup Widget build configuration (#3)
    * Add Widget extension
    
    * Add framework
    
    * Add timeout on github action

---
+++ MoneyFeedbackWidget/MoneyFeedbackWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MoneyFeedbackWidgetBundle.swift
+//  MoneyFeedbackWidget
+//
+//  Created by mzp on 10/15/25.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct MoneyFeedbackWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MoneyFeedbackWidget()
+    }
+}
