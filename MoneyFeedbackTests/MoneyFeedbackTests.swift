c3b7cac59b188f7f36fb2bb60b7b8edaaf323951
mzp <me@mzp.jp>
2025-10-15 18:43:32 -0700

    Add MoneyFeedbackTests

---
+++ MoneyFeedbackTests/MoneyFeedbackTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoneyFeedbackTests.swift
+//  MoneyFeedbackTests
+//
+//  Created by mzp on 10/15/25.
+//
+
+import Testing
+
+struct MoneyFeedbackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
